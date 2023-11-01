3efabd7ec72220a4a2ae029ce0edd0957310e740
yui-szk <yuisuzuki.017@gmail.com>
2023-11-02 00:15:49 +0900

    Corrected by review

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,6 @@
 
 import { quizData } from "./quizData";
 
-// 質問文
-const questionElm = document.getElementById("question");
-
-// 選択肢
-// const a_text = document.getElementById("a-text");
-// const b_text = document.getElementById("b-text");
-// const c_text = document.getElementById("c-text");
-// const d_text = document.getElementById("d-text");
-
 // 送信ボタン
 const submitBtn = document.getElementById("submit");
 
@@ -21,46 +12,26 @@ let currentQuiz = 0;
 // 現在のスコア
 let score = 0;
 
-//次の問題ボタン
-const nextQuizBtn = document.getElementById("next-quiz");
-
 //
 const quizHeaderElm: HTMLElement | null =
   document.getElementById("quiz-header");
 const resultsConElm = document.getElementById("results-container");
-const resultsElm = document.getElementById("results");
 
 const loadQuiz = (): void => {
   // 問題を取得
   const currentQuizData = quizData[currentQuiz];
 
   // 質問文を表示
+  const questionElm = document.getElementById("question");
   if (questionElm) {
     questionElm.innerText = currentQuizData.question;
   }
 
   // 画像を表示
   document.getElementById("quizImg")?.setAttribute("src", currentQuizData.img);
-
-  // 選択肢を表示
-  //   if (a_text && b_text && c_text && d_text) {
-  //     a_text.innerText = currentQuizData.a;
-  //     b_text.innerText = currentQuizData.b;
-  //     c_text.innerText = currentQuizData.c;
-  //     d_text.innerText = currentQuizData.d;
-  //   }
 };
 
 loadQuiz();
-
-// 選択したボタンのvalueを返す
-// const getAnswered = () => {
-//   const formElements = document.forms[0];
-//   const value = formElements.answer.value;
-//   if (typeof value === "string") {
-//     return value;
-//   }
-// };
 
 const answerElement = document.getElementById("answer-box");
 
@@ -75,6 +46,7 @@ const getAnswered = () => {
 };
 
 const showResults = (results: string): void => {
+  const resultsElm = document.getElementById("results");
   if (quizHeaderElm && submitBtn && resultsConElm && resultsElm) {
     quizHeaderElm.style.display = "none";
     submitBtn.style.display = "none";
@@ -116,18 +88,14 @@ submitBtn?.addEventListener("click", (event) => {
       showResults("残念...");
     }
 
-    // ラジオボタンの選択を解除する
-    // const answerElement = document.getElementById(answer);
-    // if (answerElement && answerElement instanceof HTMLInputElement) {
-    //   answerElement.checked = false;
-    // }
-
     if (answerElement instanceof HTMLInputElement) {
       answerElement.value = "";
     }
   }
 });
 
+//次の問題ボタン
+const nextQuizBtn = document.getElementById("next-quiz");
 nextQuizBtn?.addEventListener("click", (event) => {
   event.preventDefault();
 
